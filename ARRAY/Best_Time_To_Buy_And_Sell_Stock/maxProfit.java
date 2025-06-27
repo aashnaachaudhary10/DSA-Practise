1140f832f767cc965ad53e77b43e898908550e58
Aashnaa Chaudhary <185051375+aashnaachaudhary10@users.noreply.github.com>
2025-06-28 04:55:38 +0530

    Added Best Time to buy and sell stock

---
+++ ARRAY/Best_Time_To_Buy_And_Sell_Stock/maxProfit.java
@@ -0,0 +1,26 @@
+//problem link = https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//Approach: Greedy
+
+
+
+
+
+class maxProfit {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];  
+            } else {
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;  
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
