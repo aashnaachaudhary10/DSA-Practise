ca7d9b012a51955e9b3bda865582f6d81a26a2c5
Aashnaa Chaudhary <185051375+aashnaachaudhary10@users.noreply.github.com>
2025-07-13 10:31:14 +0530

    Added OOPS JAVA folder with Java OOPs programs

---
+++ OOPS JAVA/INHERITANCE/singleInheritance.java
@@ -0,0 +1,62 @@
+                                //EXAMPLE1
+
+
+
+import java.util.*;
+
+//superclass
+class employee{
+    int salary=60000;
+}
+
+
+//subclass
+class engineer extends employee{
+    int benefits=10000;
+}
+
+
+//driver code 
+ public class singleInheritance{
+    public static void main(String args[]){
+        engineer e1= new engineer();
+        System.out.println("Salary:" + e1.salary + "\n benefits:"+ e1.benefits);
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+                               //EXAMPLE2
+
+// import java.util.*;
+
+// class Animals{
+//     void sound(){
+//     System.out.println("Animals Sounds");
+//     }
+// }
+// class dogs extends Animals{
+//     void bark(){
+//     System.out.println("Dogs barkk");
+//     }
+// }
+
+// public class singleInheritance{
+//     public static void main(String args[]){
+//         dogs d = new dogs();
+//         d.sound();
+//         d.bark();
+//     }
+// }
+
+
+
+
+
---
+++ OOPS JAVA/singleInheritance.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+//superclass
+class employee{
+    int salary=60000;
+}
+
+
+//subclass
+class engineer extends employee{
+    int benefits=10000;
+}
+
+
+//driver code 
+ public class singleInheritance{
+    public static void main(Strings )
+ }
+
